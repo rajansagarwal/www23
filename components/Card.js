55576ead2475c349a2ccb6b9c326c0e65fcafdb7
Rajan Agarwal <rajan.ag005@gmail.com>
2024-05-28 07:48:36 -0400

    new format

--- components/Card.js
+++ components/Card.js
@@ -1,34 +1,23 @@
-import { Grid } from "@geist-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
-import FullVideo from "./FullVideo";
-import Link from "next/link"
+import Link from "next/link";
+import { Typewriter } from "react-simple-typewriter";
 
-export default function Card({
-  name,
-  description,
-  tag,
-  image,
-  link,
-}) {
+export default function Card({ name, description, tag, image, link }) {
   const [hovered, setHovered] = useState(false);
-
-  const handleHover = () => {
-    setHovered(!hovered);
-  };
-
+  if (!description) {
+    description = "rajan";
+  }
   return (
     <Link
       style={{
         width: "100%",
         maxHeight: "23.5rem",
-        // paddingBottom: "2rem",
         position: "relative",
         cursor: "pointer",
+        display: "block", // Make sure the Link is block level to contain the absolutely positioned elements
       }}
-      // onClick={() => window.open(link)}
       href={link}
-      className="small-big"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
@@ -36,16 +25,41 @@ export default function Card({
         src={image}
         width={500}
         height={300}
-        className="rounded-lg"
         style={{
           height: "100%",
           width: "100%",
           aspectRatio: "4 / 3",
           objectFit: "cover",
+          borderRadius: "5px",
           objectPosition: "center",
-          opacity: hovered ? 1 : 0.9,
+          transition: "opacity 0.5s ease",
+          opacity: hovered ? 0.5 : 1,
         }}
       />
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0.5rem",
+          width: "95%",
+          color: "white",
+          padding: "1rem",
+          borderRadius: "5px",
+          display: hovered ? "block" : "none",
+        }}
+      >
+        {hovered && (
+          <Typewriter
+            words={description.split()}
+            loop={1}
+            cursor
+            cursorStyle="|"
+            typeSpeed={10}
+            deleteSpeed={500}
+            delaySpeed={1000}
+          />
+        )}
+      </div>
       <div
         style={{
           position: "absolute",
@@ -54,7 +68,7 @@ export default function Card({
         }}
       >
         <p className="text-white font-bricolage-grotesque h-full w-full px-1 bg-slate-500 bg-clip-padding backdrop-filter backdrop-blur bg-opacity-30 saturate-50 backdrop-contrast-100 rounded-sm">
-          <span>{name}</span>
+          {name}
         </p>
       </div>
     </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,154 @@
-"use client";
-
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import { Grid } from "@geist-ui/react";
 import { useRouter } from "next/router";
 import Card from "@/components/Card";
-import { useEffect, useState } from "react";
-
-// const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  //   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState([]);
 
-  //   useEffect(() => {
-  //     setLoading(false);
-  //   }, []);
+  // Define a list of cards with a category for each card
+  const cards = [
+    {
+      categories: ["Software"],
+      name: "Train Automation & Signal Processing",
+      link: "/work/train-automation",
+      image: "/nyct.png",
+      description:
+        "Interning at Hitachi Rail to work on train automation and critical safety systems. Currently building the company's first simulation software using train logs and signal processing",
+    },
+    {
+      categories: ["LLMs", "Software"],
+      name: "Embeddings Visualization",
+      link: "/work/matchmaking",
+      image: "/graph-talking.png",
+      description:
+        "An open sourced matchmaking graphs at events to visualize the connections between people and their interests. Built using LLMs and graph embeddings and used on 700+ person events.",
+    },
+    {
+      categories: ["Robotics", "Software", "Machine Learning"],
+      name: "Self-Driving Tank",
+      link: "/work/tank",
+      image: "/boat.png",
+      description:
+        "A self-driving RC tank that delivers food around campus. We combined perception, world modelling and motion planning strategies. Featured by OpenCV.",
+    },
+    {
+      categories: ["LLMs", "Software", "Machine Learning", "Experiments"],
+      name: "LLM Memory Architecture",
+      link: "https://www.youtube.com/watch?v=Vmupugz2M1s&t=1s&ab_channel=RajanAgarwal",
+      image: "/portal-side.png",
+      description:
+        "An AI Video Wearables to ambiently compute your life and store it in a knowledge graph for inference. Winner of Hack the North 2023 and presented at Socratica Symposium",
+    },
+    {
+      categories: ["Robotics", "Software", "Machine Learning", "Experiments"],
+      name: "Visual Reasoning for Robotics",
+      link: "/",
+      image: "/depth-distances.png",
+      description: "End-to-end neural networks for visual reasoning in dynamic, changing scenarios. We used depth and optical flow algorithms to predict the future of the environment.",
+    },
+    {
+      categories: ["Experimentation", "Software", "Machine Learning", "LLMs"],
+      name: "Reverse Engineering Earthquakes",
+      link: "https://shapeshift.space",
+      image: "/shapeshift-talk.png",
+      description: "Optimization of earthquake-resistant buildings using novel fourier algorithms, as well as policy analysis with RAG, reranking and LLMs."
+    },
+    {
+      categories: ["Experiments", "Software", "Machine Learning"],
+      name: "Research Public Funding Model",
+      link: "https://devpost.com/software/connexsci-20nrjy",
+      image: "/connexsci_shots.png",
+      description: "Funding model for public investments in research, using knowledge graphs and distributed investments. Winner of Hack the North 2022."
+    },
+    {
+      categories: ["LLMs", "Software", "Experiments"],
+      name: "Generative UI",
+      link: "https://tempolabs.ai",
+      image: "/tempo.png",
+      description: "Worked with Tempo Labs to build generative UI from scratch."
+    },
+    {
+      categories: ["Robotics", "Software", "Machine Learning"],
+      name: "Self Driving Vehicle",
+      link: "https://www.watonomous.ca/",
+      image: "/watonomous.png",
+      description: "A self-driving car design team in Waterloo. Working on the Motion Planning & Control team."
+    },
+    {
+      categories: ["LLMs", "Software", "Experiments"],
+      name: "Recommendation Engine",
+      link: "https://aviato.co",
+      image: "/aviato.png",
+      description: "A recommendation engine for hiring AI engineers, contract work with Aviato."
+    },
+    {
+      categories: ["Software", "Experiments"],
+      name: "Offline Mesh Network",
+      link: "https://github.com/rajansagarwal/zephyr",
+      image: "/zephyr-pic.png",
+      description: "An offline mesh using multi peer connectivity to send encrypted messages between devices in your vicinity without internet."
+    },
+    {
+      categories: ["Software"],
+      name: "Restoring Digital Humanity",
+      link: "https://www.linkedin.com/company/campsocial/",
+      image: "/cs_shots.png",
+      description: "Internship to work on restroring humanity online. I worked primarily on the rewards platform, in Solidity, Ruby and Typescript."
+    },
+    {
+      categories: ["Experiments"],
+      name: "Half Marathon",
+      link: "/",
+      image: "/half-marathon.png",
+      description: "Ran a half marathon the day before my midterms!"
+    },
+    {
+      categories: ["Experiments"],
+      name: "The Platonist: Fictional Novel",
+      link: "https://www.amazon.ca/Platonist-Awake-Dreaming-Rajan-Agarwal/dp/B0BRDD3QLQ/ref=sr_1_1?crid=2QCBOE10VAEAW&keywords=the+platonist+rajan&qid=1706422953&s=books&sprefix=the+platonist+rajan%2Cstripbooks%2C505&sr=1-1",
+      image: "https://z.rajan.sh/collection.png",
+      description: "Wrote a novel about a high school student in the context of the works of Plato."
+    },
+    {
+      categories: ["Software", "Experiments"],
+      name: "Knowledge Repositories",
+      description:
+        "A school-wide platform for club management and knowledge sharing, through repositories, content, announcements and quizzes.",
+      link: "https://github.com/rajansagarwal",
+      image: "/homepage_aire_shots.png",
+      description: "School-wide club management platform for knowledge sharing, through repositories, content, announcements and quizzes."
+    },
+  ];
+
+  const handleFilterChange = (category) => {
+    setFilters((currentFilters) => {
+      if (currentFilters.includes(category)) {
+        return currentFilters.filter((f) => f !== category);
+      } else {
+        return [...currentFilters, category];
+      }
+    });
+  };
+
+  const categories = [
+    "Robotics",
+    "Software",
+    "LLMs",
+    "Machine Learning",
+    "Experiments",
+    // "Writings",
+  ];
+
   return (
-    <main
-      style={{
-        padding: "2rem 2rem",
-      }}
-    >
+    <main style={{ padding: "2rem 2rem" }}>
       <Head>
-        <title>
-          Rajan Agarwal
-        </title>
+        <title>Rajan Agarwal</title>
         <meta
           name="description"
-          content="Rajan Agarwal is an 18-year-old Software Engineer studying at the University of Waterloo. Explore his work on Shapeshift, a due-diligence optimization platform for earthquake-resistant buildings. Check out his experience at Arterial, Camp Social, and Hack Club. Rajan is seeking work opportunities for Summer 2024."
+          content="Rajan Agarwal is an 18-year-old Software Engineer studying at the University of Waterloo. He is currently working on AI for systems, both digital and embedded, to better understand and interact with our digital and physical world. He has built visual reasoning for robotics, earthquake-resistant building optimization, self driving cars and written novels about philosophy."
         />
         <meta
           name="keywords"
@@ -39,160 +159,62 @@ export default function Home() {
           href="https://em-content.zobj.net/thumbs/160/apple/81/seedling_1f331.png"
         />
       </Head>
-      <div
-        styles={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div className="pb-10">
-          <h2 className="font-serif-variation mt-3 mb-5 font-bricolage-grotesque text-4xl text-white font-normal md:text-3xl lg:text-4xl">
-            Rajan Agarwal
-          </h2>
-          <p className="text-neutral-400 text-lg font-bricolage-grotesque w-[100%] max-w-[500px]">
-            AI for systems, both digital and embedded, to better understand and
-            interact with our digital and physical world.
-          </p>
-          <p className="text-neutral-400 text-sm font-bricolage-grotesque">
-            <a
-              className="text-neutral-300 fade-in-3"
-              href="https://se-webring.xyz/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Software Engineering @ Waterloo.
-            </a>
-            <br />
-            Train Automation @ Hitachi Rail. World Modelling @ Watonomous
-          </p>
-        </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Grid.Container justify="center" gap={2}>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Train Automation & Signal Processing"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="/work/train-automation"
-              image="/nyct.png"
-            />
+          <Grid xs={24} md={16} lg={12} xl={12}>
+            <div className="pb-10">
+              <h2 className="font-serif-variation mt-3 mb-5 font-bricolage-grotesque text-4xl text-white font-normal md:text-3xl lg:text-4xl">
+                Rajan Agarwal
+              </h2>
+              <p className="text-neutral-400 text-lg font-bricolage-grotesque w-[100%] max-w-[500px]">
+                AI for systems, both digital and embedded, to better understand
+                and interact with our digital and physical world.
+              </p>
+              <p className="text-neutral-400 text-sm font-bricolage-grotesque">
+                <a
+                  className="text-neutral-300 fade-in-3"
+                  href="https://se-webring.xyz/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Software Engineering @ Waterloo.
+                </a>
+                <br />
+                Train Automation @ Hitachi Rail. World Modelling @ Watonomous
+              </p>
+              {categories.map((category) => (
+                <button
+                  key={category}
+                  className={`p-2 mr-2 mb-2 rounded-md hover:bg-zinc-800 ${
+                    filters.includes(category) ? "bg-zinc-800" : "bg-zinc-900"
+                  }`}
+                  onClick={() => handleFilterChange(category)}
+                >
+                  {category}
+                </button>
+              ))}
+            </div>
           </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Embeddings Visualization"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="/work/matchmaking"
-              image="/graph-talking.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Self-Driving Tank"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="/work/tank"
-              image="/boat.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="LLM Memory Architecture"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="https://www.youtube.com/watch?v=Vmupugz2M1s&t=1s&ab_channel=RajanAgarwal"
-              image="/portal-side.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Visual Reasoning for Robotics"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="/"
-              image="/depth-distances.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Reverse Engineering Earthquakes"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="https://shapeshift.space"
-              image="/shapeshift-talk.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Research Public Funding Model"
-              description="Connexsci is a grant funding platform that generates exposure to undervalued and independent research through graph-based analytics. Winner of Hack the North 2022."
-              link="https://devpost.com/software/connexsci-20nrjy"
-              image="/connexsci_shots.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Generative UI"
-              description="Zephyr is an offline mesh network that uses neighbouring devices as P2P nodes for long-distance interaction, for both simple messages and complex POST-requests"
-              link="https://tempolabs.ai"
-              image="/tempo.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Self Driving Vehicle"
-              description="As a Software Engineer at Camp Social, I worked to help digital communities coordinate efforts, share resources, and achieve positive-sum outcomes. Working with Tyepscript, Ruby on Rails and Solidity, I wrote thousands of lines of production code."
-              link="https://www.watonomous.ca/"
-              image="/watonomous.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Recommendation Engine"
-              description="A Self-Driving Tank that can deliver food anywhere on the Waterloo campus, using PID, computer vision and custom neural networks for pathfinding. Built from scratch for the Waterloo Software Engineering 1A Design Project."
-              link="https://aviato.co"
-              image="/aviato.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Offline Mesh Network"
-              description="Zephyr is an offline mesh network that uses neighbouring devices as P2P nodes for long-distance interaction, for both simple messages and complex POST-requests"
-              link="https://github.com/rajansagarwal/zephyr"
-              image="/zephyr-pic.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Restoring Digital Humanity"
-              description="As a Software Engineer at Camp Social, I worked to help digital communities coordinate efforts, share resources, and achieve positive-sum outcomes. Working with Tyepscript, Ruby on Rails and Solidity, I wrote thousands of lines of production code."
-              link="https://www.linkedin.com/company/campsocial/"
-              image="/cs_shots.png"
-            />
-          </Grid>
-          {/* <Grid xs={24} md={8} lg={6} xl={4}>
-              <Articles />
-            </Grid> */}
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Half Marathon"
-              description="Connexsci is a grant funding platform that generates exposure to undervalued and independent research through graph-based analytics. Winner of Hack the North 2022."
-              link="/"
-              image="/half-marathon.png"
-            />
-          </Grid>
-          {/* <Grid xs={24} md={8} lg={6} xl={4}>
-              <Stats />
-            </Grid> */}
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="The Platonist: Fictional Novel"
-              description="A fictional novel that navigates the works of Plato's Republik through the story of a young boy, Ryan. The novel was noted a Top 20 Philosophy Book in North America."
-              link="https://www.amazon.ca/Platonist-Awake-Dreaming-Rajan-Agarwal/dp/B0BRDD3QLQ/ref=sr_1_1?crid=2QCBOE10VAEAW&keywords=the+platonist+rajan&qid=1706422953&s=books&sprefix=the+platonist+rajan%2Cstripbooks%2C505&sr=1-1"
-              image="https://z.rajan.sh/collection.png"
-            />
-          </Grid>
-          <Grid xs={24} md={8} lg={6} xl={4}>
-            <Card
-              name="Knowledge Repositories"
-              description="A school-wide platform for club management and knowledge sharing, through repositories, content, announcements and quizzes."
-              image="/homepage_aire_shots.png"
-              link="https://github.com/rajansagarwal"
-            />
-          </Grid>
+
+          {cards
+            .filter(
+              (card) =>
+                filters.length === 0 ||
+                (card.categories &&
+                  card.categories.some((category) =>
+                    filters.includes(category)
+                  ))
+            )
+            .map((card, index) => (
+              <Grid key={index} xs={24} md={8} lg={6} xl={4}>
+                <Card
+                  name={card.name}
+                  link={card.link}
+                  image={card.image}
+                  description={card.description}
+                />
+              </Grid>
+            ))}
         </Grid.Container>
         <br />
         <br />
